4d00779bbda5bdc3901cd7e3c1c78d74a3457b11
d3vyatk4ru <danya.devyatkin@mail.ru>
2021-10-26 11:45:18 +0300

    Added the headers of project

---
+++ include/stucture_t.h
@@ -0,0 +1,16 @@
+//
+// Created by d3vyatk4 on 24.10.2021.
+//
+
+#ifndef HOMEWORK_2_TECHNO_PARK_STUCTURE_T_H
+#define HOMEWORK_2_TECHNO_PARK_STUCTURE_T_H
+
+typedef struct args_routine {
+    char symbol;
+    char *region;
+    long file_len;
+    unsigned int count;
+} args_routine_t;
+
+
+#endif //HOMEWORK_2_TECHNO_PARK_STUCTURE_T_H
